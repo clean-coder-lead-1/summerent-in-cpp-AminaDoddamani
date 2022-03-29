c0eb42a393b1b9224e1002bc287c5c5ae1c09ab6
AminaDoddamani <102234973+AminaDoddamani@users.noreply.github.com>
2022-03-29 20:55:58 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -10,5 +10,5 @@ TEST_CASE("infers the breach according to limits") {
 }
 
 TEST_CASE("classifyTemperaturebreach validate return value") {
-  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING,0) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING,0) == NORMAL);
 }
